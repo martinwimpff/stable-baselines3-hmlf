c8be25a5934a5d4ab90c8b16c23971a43ccd9416
ac132249 <ac132249@uni-stuttgart.de>
2022-03-07 16:01:18 +0100

    added circular imports test

---
+++ tests/test_circular_imports.py
@@ -0,0 +1,97 @@
+def test_circular_imports_atari():
+    from stable_baselines3.common import atari_wrappers
+
+
+def test_circular_imports_base_class():
+    from stable_baselines3.common import base_class
+
+
+def test_circular_imports_buffers():
+    from stable_baselines3.common import buffers
+
+
+def test_circular_imports_callbacks():
+    from stable_baselines3.common import callbacks
+
+
+def test_circular_imports_distributions():
+    from stable_baselines3.common import distributions
+
+
+def test_circular_imports_env_checker():
+    from stable_baselines3.common import env_checker
+
+
+def test_circular_imports_env_util():
+    from stable_baselines3.common import env_util
+
+
+def test_circular_imports_evaluation():
+    from stable_baselines3.common import evaluation
+
+
+def test_circular_imports_logger():
+    from stable_baselines3.common import logger
+
+
+def test_circular_imports_monitor():
+    from stable_baselines3.common import monitor
+
+
+def test_circular_imports_noise():
+    from stable_baselines3.common import noise
+
+
+def test_circular_imports_off_policy_algorithm():
+    from stable_baselines3.common import off_policy_algorithm
+
+
+def test_circular_imports_on_policy_algorithm():
+    from stable_baselines3.common import on_policy_algorithm
+
+
+def test_circular_imports_policies():
+    from stable_baselines3.common import policies
+
+
+def test_circular_imports_preprocessing():
+    from stable_baselines3.common import preprocessing
+
+
+def test_circular_imports_results_plotter():
+    from stable_baselines3.common import results_plotter
+
+
+def test_circular_imports_running_mean_std():
+    from stable_baselines3.common import running_mean_std
+
+
+def test_circular_imports_save_util():
+    from stable_baselines3.common import save_util
+
+
+def test_circular_imports_torch_layers():
+    from stable_baselines3.common import torch_layers
+
+
+def test_circular_imports_type_aliases():
+    from stable_baselines3.common import type_aliases
+
+
+def test_circular_imports_utils():
+    from stable_baselines3.common import utils
+
+
+def test_circular_imports_vec_env():
+    from stable_baselines3.common.vec_env import (
+        base_vec_env,
+        dummy_vec_env,
+        obs_dict_wrapper,
+        subproc_vec_env,
+        util,
+        vec_check_nan,
+        vec_frame_stack,
+        vec_normalize,
+        vec_transpose,
+        vec_video_recorder,
+    )
