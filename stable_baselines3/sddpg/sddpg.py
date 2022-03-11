cddc696f13e02cff1373cf6d6b470f3f7b5d3974
ac132249 <ac132249@uni-stuttgart.de>
2022-03-11 09:38:03 +0100

    typing bugfix

--- stable_baselines3/sddpg/sddpg.py
+++ stable_baselines3/sddpg/sddpg.py
@@ -77,7 +77,7 @@ class SDDPG(OffPolicyAlgorithm):
         target_noise_clip: float = 0.5,
         tensorboard_log: Optional[str] = None,
         create_eval_env: bool = False,
-        policy_kwargs: Dict[str, Any] = None,
+        policy_kwargs: Optional[Dict[str, Any]] = None,
         verbose: int = 0,
         seed: Optional[int] = None,
         device: Union[th.device, str] = "auto",
